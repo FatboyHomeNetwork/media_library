6a50d037812f4b85bffc8dc097e84340e5e07d3f
Paulp <fatboyhomenetwork@gmail.com>
2023-01-18 15:18:34 +1100

    .

--- media_library/media_library_manager/media_library_manager.py
+++ media_library/media_library_manager/media_library_manager.py
@@ -2,18 +2,40 @@ import logging
 import os 
 import sys 
 
-import media_library.media_library_manager.media_library as ml
+from enum import Enum    
 
-from media_library.Import_item import queue
+#import Import_item.import_queue
 from media_library.Import_item import importer 
 
+class mime_type(Enum):
+    IMAGE = 1
+    VIDEO = 2
+    AUDIO = 3
+    TEXT = 4
+        
+    NOT_SUPPORTED = 99
 
 class media_library_manager:
 
     def __init__(self, library_path):
         
-        self.queue = queue(ml.queue_file(library_path))
-        logging.basicConfig(filename=ml.log_file(library_path), format='%(asctime)s %(message)s', encoding='utf-8', level=logging.DEBUG)
+        self.library_path = library_path
+        self.queue = Import_item.import_queue.import_queue(self.queue_file(library_path))
+        
+        logging.basicConfig(filename=self.log_file(library_path), format='%(asctime)s %(message)s', encoding='utf-8', level=logging.DEBUG)
+
+
+    def log_file(self):
+        return os.path.join(self.library_path, 'system', 'media_library.log')
+
+    def tmp_path(self):
+        return os.path.join(self.library_path, 'system','temp')
+
+    def queue_file(self):
+        return os.path.join(self.library_path, 'system','import.queue')
+
+    def media_path(self):
+        return os.path.join(self.library_path, 'media')
 
     
     def queue(self, item_path):
--- media_library/media_library_manager/media_library_manager_TESTER.py
+++ media_library/media_library_manager/media_library_manager_TESTER.py
@@ -1,9 +1,9 @@
-from import_queue_manager import import_queue_manager
 
+import media_library_manager as mlm
 
 if __name__ == "__main__":
     
-    IMPORT_QUEUE_PATH = '\\\\SERVER\\Users\\Paul\\Documents\\Projects\\Fat Boy Home Network (FBHN)\\01 Media Library\\50 TestEnvironment\\media_library'
+    MEDIA_LIBRARY = '\\\\SERVER\\Users\Paul\\Documents\\GitHub\\media_library\\test\\media_library'
     
     # An existing share
     #PATH = '\\\\SERVER\\Users\\Paul\\Documents\\Projects\\Fat Boy Home Network (FBHN)\\01 Media Library\\50 TestEnvironment\\_test_data\\five'
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     # A local drive mapped to an admin share unc path 
     PATH = "C:\\intel"
     
-    import_queue_manager = import_queue_manager(IMPORT_QUEUE_PATH)
-    import_queue_manager.add(PATH)
+    mlm = media_library_manager(IMPORT_QUEUE_PATH)
+    mlm.queue(PATH)
